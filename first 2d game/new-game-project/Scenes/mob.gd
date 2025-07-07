69604dbeddce0577bdbc4a68b53810b146b8ec43
tuvshin.erdene25@gmail.com <tuvshin.erdene25@gmail.com>
2025-07-07 13:05:22 +0800

    fixed script

--- first 2d game/new-game-project/Scenes/mob.gd
+++ first 2d game/new-game-project/Scenes/mob.gd
@@ -3,8 +3,10 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("Script successfully loaded!")
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
+	print($AnimatedSprite2D.animation)
 	$AnimatedSprite2D.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
